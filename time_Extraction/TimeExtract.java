de6eecca8d5d64f8f0b24a2edbd7d5cf6c778a77
Kailun Wu <kailun@me.com>
2015-05-05 16:26:43 -0400

    blah

---
+++ time_Extraction/TimeExtract.java
@@ -0,0 +1,39 @@
+package time_Extraction;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class TimeExtract {
+    private List<String> absoluteRegexs;
+    private List<String> relativeRegexs;
+
+    public Matcher() {
+        absoluteRegexs = IO.readRegex("./TimeRegexAbsolute.txt");
+        relativeRegexs = IO.readRegex("./TimeRegexRelative.txt");
+    }
+    
+    public List<MatchedTime> extractText(String text) {
+        List<MatchedTime> result = new ArrayList<MatchedTime>();
+        for (String regex : absoluteRegexs) {
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find()) {
+                MatchedTime matchedTime = new MatchedTime(TYPE.ABSOLUTE),
+                    regex, matcher.group());
+                result.add(matchedTime);
+            }
+        }
+        for (String regex : relativeRegexs) {
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find()) {
+                MatchedTime matchedTime = new MatchedTime(TYPE.RELATIVE),
+                    regex, matcher.group());
+                result.add(matchedTime);
+            }
+        }
+        return result;
+    }
+}
