a2ae38ab6b5c75bc9fe9a4112bf93d19be977009
Kailun Wu <kailun@me.com>
2015-05-05 16:18:02 -0400

    Implement Matcher

--- time_Extraction/Matcher.java
+++ time_Extraction/Matcher.java
@@ -3,9 +3,34 @@ package time_Extraction;
 import java.util.List;
 
 public class Matcher {
-    private List<String> regexList;
+    private List<String> absoluteRegexs;
+    private List<String> relativeRegexs;
+
+    public Matcher() {
+        absoluteRegexs = readRegex("./TimeRegexAbsolute.txt");
+        relativeRegexs = readRegex("./TimeRegexRelative.txt");
+    }
     
-    public List<MatchedTime> match(String article){
-        return null;
+    public List<MatchedTime> extractText(String text) {
+        List<MatchedTime> result = new ArrayList<MatchedTime>();
+        for (String regex : absoluteRegexs) {
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find()) {
+                MatchedTime matchedTime = new MatchedTime(TYPE.ABSOLUTE),
+                    regex, matcher.group());
+                result.add(matchedTime);
+            }
+        }
+        for (String regex : relativeRegexs) {
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find()) {
+                MatchedTime matchedTime = new MatchedTime(TYPE.RELATIVE),
+                    regex, matcher.group());
+                result.add(matchedTime);
+            }
+        }
+        return result;
     }
 }
