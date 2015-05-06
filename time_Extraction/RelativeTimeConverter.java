acf0584fbc1a96501e8182292cbef18c4433581b
hj690 <hongdaj@yahoo-inc.com>
2015-05-05 22:32:52 -0400

    added date formats

--- time_Extraction/RelativeTimeConverter.java
+++ time_Extraction/RelativeTimeConverter.java
@@ -11,28 +11,59 @@ public class RelativeTimeConverter {
     }
     
     public Calendar convert(String relativeTime){
-        Calendar result = null;
+        Calendar result = referenceTime;
         if(relativeTime.contains("today")){
-            result = referenceTime;
+            return result;
         }
         if(relativeTime.contains("yesterday")){
-            
+            result.add(Calendar.DATE, -1);
+            return result;
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        if(result != null){
-            System.out.println("Date: '" + result.toString() + "' convertered from: " + relativeTime);
+        if(relativeTime.contains("tomorrow")){
+            result.add(Calendar.DATE, 1);
+            return result;
         }
-        else{
-            System.out.println("Can't concert this relative time: " + relativeTime);
+        if(relativeTime.contains("next month") || relativeTime.contains("a month later")){
+            result.add(Calendar.MONTH, 1);
+            return result;
         }
-        return result;
+        if(relativeTime.contains("last month") || relativeTime.contains("a month ago")){
+            result.add(Calendar.MONTH, -1);
+            return result;
+        }
+        if(relativeTime.contains("next year")){
+            result.add(Calendar.YEAR, 1);
+            return result;
+        }
+        if(relativeTime.contains("last year") || relativeTime.contains("past year")
+                || relativeTime.contains("year ago") || relativeTime.contains("year earlier")){
+            result.add(Calendar.MONTH, -1);
+            return result;
+        }
+        if(relativeTime.contains("years ago") || relativeTime.contains("years earlier")){
+            int year = Integer.parseInt(relativeTime.replaceAll("[^0-9]", ""));
+            result.add(Calendar.YEAR, year*-1);
+            return result;
+        }
+        if(relativeTime.contains("years later")){
+            int year = Integer.parseInt(relativeTime.replaceAll("[^0-9]", ""));
+            result.add(Calendar.YEAR, year);
+            return result;
+        }
+        if(relativeTime.contains("months ago") || relativeTime.contains("months earlier")){
+            int year = Integer.parseInt(relativeTime.replaceAll("[^0-9]", ""));
+            result.add(Calendar.MONTH, year*-1);
+            return result;
+        }
+        if(relativeTime.contains("months later")){
+            int year = Integer.parseInt(relativeTime.replaceAll("[^0-9]", ""));
+            result.add(Calendar.MONTH, year);
+            return result;
+        }
+
+      
+        System.out.println("Can't concert this relative time: " + relativeTime);
+       
+        return null;
     }
 }
